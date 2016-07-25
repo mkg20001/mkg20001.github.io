74a10836f329cbbd9b228acb82ad136bcd57a860
mkg20001 <mkg20001@gmail.com>
2016-07-25 17:20:00 +0200

    Deploy with travis

--- gulpfile.js
+++ gulpfile.js
@@ -141,9 +141,13 @@ gulp.task("ipfs",["clean"], function() {
 });
 
 gulp.task("publish", function() {
-  return gulp.src('./dist/**/*')
-  .pipe($.ghPages({
-    branch:"master",
-    force:"true"
-  }));
+  return gulp.src('dist/**/*')
+    // Check if running task from Travis CI, if so run using GH_TOKEN
+    // otherwise run using ghPages defaults.
+    .pipe($.if(process.env.TRAVIS === 'true', $.ghPages({
+      remoteUrl: 'https://$GH_TOKEN@github.com/mkg20001/mkg20001.github.io.git',
+      silent: true,
+      branch: 'master',
+      force:true
+    }), $.ghPages()));
 });
