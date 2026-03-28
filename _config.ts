35ee33009d5938d1f3f2a2fc10f90f43b0ad5616
Maciej Krüger <mkg20001@gmail.com>
2026-03-28 19:48:57 +0100

    feat: lume site

---
+++ _config.ts
@@ -0,0 +1,48 @@
+import lume from "lume/mod.ts";
+import date from "lume/plugins/date.ts";
+import code_highlight from "lume/plugins/code_highlight.ts";
+import decap_cms from "lume/plugins/decap_cms.ts";
+import multilanguage from "lume/plugins/multilanguage.ts";
+import robots from "lume/plugins/robots.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+import terser from "lume/plugins/terser.ts";
+import google_fonts from "lume/plugins/google_fonts.ts";
+import sass from "lume/plugins/sass.ts";
+import unocss from "lume/plugins/unocss.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import source_maps from "lume/plugins/source_maps.ts";
+import check_urls from "lume/plugins/check_urls.ts";
+import favicon from "lume/plugins/favicon.ts";
+import svgo from "lume/plugins/svgo.ts";
+import transform_images from "lume/plugins/transform_images.ts";
+import sri from "lume/plugins/sri.ts";
+import validate_html from "lume/plugins/validate_html.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import minify_html from "lume/plugins/minify_html.ts";
+import brotli from "lume/plugins/brotli.ts";
+
+const site = lume();
+
+site.use(date());
+site.use(code_highlight());
+site.use(decap_cms());
+site.use(multilanguage());
+site.use(robots());
+site.use(esbuild());
+site.use(terser());
+site.use(google_fonts());
+site.use(sass());
+site.use(unocss());
+site.use(tailwindcss());
+site.use(source_maps());
+site.use(check_urls());
+site.use(favicon());
+site.use(svgo());
+site.use(transform_images());
+site.use(sri());
+site.use(validate_html());
+site.use(sitemap());
+site.use(minify_html());
+site.use(brotli());
+
+export default site;
